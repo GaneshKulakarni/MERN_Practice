9c27e32cde38e13831e852bee31629ea9776a23c
Ganesh Kulakarni <ganeshkulakarni124@gmail.com>
2026-03-06 10:25:16 +0530

    added dashboard.ejs in Node.js folder

--- Node.js/page34.js
+++ Node.js/page34.js
@@ -1,12 +1,24 @@
+import { name } from 'ejs';
 import express from 'express'
 const app = express();
 app.set('view engine',"ejs")
 app.set("views","views")
 app.listen(8080,()=>{console.log("server is running on the port 8080")})
 
+
+const users =[
+    {name:"ganesh",email:"ganesh123@gmail.com",password:"232434"},
+    {name:"suresh",email:"suresh343@gmail.com",password:"736203"},
+    {name:"sai reddy",email:"saireddy565@gmail.com",password:"847362"}
+]
+
 app.get('/login',(req,res)=>{
     res.render("login")
 })
 app.get('/register',(req,res)=>{
     res.render("Register")
+})
+
+app.get('/',(req,res)=>{
+    res.render("Dashboard")
 })
